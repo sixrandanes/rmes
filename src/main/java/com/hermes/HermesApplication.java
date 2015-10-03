2026b276bb52040a239da36b2830542cf4b3905d
Sylvain <lbsylvain1@aol.com>
2015-10-04 10:55:47 +1100

    first commit project hermes

---
+++ src/main/java/com/hermes/HermesApplication.java
@@ -0,0 +1,16 @@
+package com.hermes;
+/**
+ * Created by Sylvain on 02/10/15.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HermesApplication.class, args);
+    }
+}
