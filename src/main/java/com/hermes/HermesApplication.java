8422e1d4545d6df308130e2dcccd60777803afb0
Sylvain <sylvain.leborgne@gmail.com>
2015-10-04 14:22:34 +1100

    enable aspectJ

--- src/main/java/com/hermes/HermesApplication.java
+++ src/main/java/com/hermes/HermesApplication.java
@@ -5,8 +5,10 @@ package com.hermes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class HermesApplication {
 
     public static void main(String[] args) {
